d2c4aeb99aa2e786aaa8d75a41d8737c2a8c6af6
Yumi Yukimura <me.cafebabe@gmail.com>
2026-01-27 22:31:25 +0530

    ferrari: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946358012694401.xml
 
-$(call soong_config_set,qtidisplay,oplus_udfps,true)
+$(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 $(call soong_config_set,qtidisplay,pxlw_vendor_namespace,vendor/oneplus/sm8450-common)
 
 # Fingerprint
